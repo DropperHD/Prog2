587bfdc8c5b9cfb84e888e9e6fbb7215d8e4ea6e
DropperHD <30983123+DropperHD@users.noreply.github.com>
2020-05-21 18:47:58 +0200

    Update LengthConverter.java

--- Prog2-2/Length/LengthConverter.java
+++ Prog2-2/Length/LengthConverter.java
@@ -1,15 +1,15 @@
 public class LengthConverter{
     /**
-     * Converts a Length into other Units
-     * @param args Value, Unit, Desired Unit
+     * Rechnet eine Länge in eine andere um und gibt sie auf der Konsole aus
+     * @param args Wert, Einheit, gewünschte Einheit
      */
     public static void main(String[] args) {
-        if (args.length != 3 ) {
-            throw new IllegalArgumentException("too much or too little input");
+        if (args.length != 3 ) { //Programm braucht genau 3 Inputs
+            throw new IllegalArgumentException("Programm braucht 3 Inputs");
         }
-        Length length = new Length(Double.parseDouble(args[0]), LengthUnit.fromSymbol(args[1]));
-        System.out.print(length.as(LengthUnit.fromSymbol(args[2])));
-    }
+        Length length = new Length(Double.parseDouble(args[0]), LengthUnit.fromSymbol(args[1])); //Neues Length Object wird erstellt
+        System.out.print(length.as(LengthUnit.fromSymbol(args[2])));                             //Args 2 muss in double umgewandelt werden,
+    }                                                                                            //da es noch ein String ist.
 }
 
 
