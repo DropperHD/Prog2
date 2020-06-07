5d2f109c9dc558c3c28953684e95cf6ec6e9a1af
DropperHD <30983123+DropperHD@users.noreply.github.com>
2020-06-07 23:04:28 +0200

    h

--- Prog2-3/Median/src/com/company/Median.java
+++ Prog2-3/Median/src/com/company/Median.java
@@ -4,20 +4,20 @@ import java.util.Arrays;
 
 public class Median {
 
-    public static double computeMedian(int ... ints){
+    public static double computeMedian(int ... ints){ //int...ints ist ein vararg und kann so viele int speichern wie man will
 
-        if(ints == null){
+        if(ints == null){ //ints darf nicht null sein
             throw new IllegalArgumentException("Argument darf nicht null sein");
         }
-        if(ints.length == 0){
+        if(ints.length == 0){ //ints darf nicht leer sein
             throw new IllegalArgumentException("Funktion braucht Argumente");
         }
 
-        Arrays.sort(ints);
-        switch (ints.length % 2 ){
-            case 1: return ints[ints.length/2];
-            case 0: return (ints[ints.length/2-1] + ints[ints.length/2])/2.0;
-            default: throw new IllegalArgumentException("Irgendwas ist schief gelaufen");
+        Arrays.sort(ints); //Sortiert das Array
+        switch (ints.length % 2 ){              //falls modulo 2 = 0 ist ist das Ergebniss gerade
+            case 1: return ints[ints.length/2]; // gibt einfach den Wert des Eintages in der mitte zurück
+            case 0: return (ints[ints.length/2-1] + ints[ints.length/2])/2.0; //gibt den mittleren Wert der beiden mittigen Einträge zurück
+            default: throw new IllegalArgumentException("Irgendwas ist schief gelaufen"); //default case wird benötigt
         }
     }
 }
--- Prog2-3/worldjumble/src/com/company/WordJumble.java
+++ Prog2-3/worldjumble/src/com/company/WordJumble.java
@@ -4,24 +4,33 @@ import java.util.Scanner;
 
 public class WordJumble
 {
+  /**
+   * Bringt ein einziges Wort durcheinander
+   *
+   * @param word Der String der verändert werden soll. darf nicht null sein
+   * @param builder Stringbuilder der das Wort dann wieder an den Text anhängt. darf auch nicht null sein
+   */
   public static void jumbleWord(String word, StringBuilder builder){
 
-    if(word == null){
-      throw new IllegalArgumentException();
+    if(word == null){                                                     //Überprüft die Eingaben
+      throw new IllegalArgumentException("word darf nicht null sein!");
+    }
+
+    if(builder == null){
+      throw new IllegalArgumentException("builder darf nicht null sein!");
     }
 
     if(word.length()<3){
       builder.append(word);
       return;
+    }                                            //zweiter Stringbuilder um das Wort zu verändern
+    StringBuilder sb = new StringBuilder(word);  //
+    for(int i = 1; i < word.length()-2;i+=2){    //Es wird vom zweiten Buchstaben bis zum vor-vorletzen durchgegangen
+      sb.insert(i+2,sb.charAt(i));         //Da es immer in Zweierpaaren passiert,wird zuerst der erste Buchstabe
+      sb.deleteCharAt(i);                        //nach dem zweiten kopiert und dann der Orginale gelöscht
     }
-    StringBuilder sb = new StringBuilder(word);
-    int length = word.length();
-    for(int i = 1; i < length-2;i+=2){
-      sb.insert(i+2,sb.charAt(i));
-      sb.deleteCharAt(i);
-    }
-    word = sb.toString();
-    builder.append(word);
+    word = sb.toString();                        //sb.toString gibt dann den veräderten String aus und kopiert diesen wieder in word
+    builder.append(word);                        //am Ende wird an das Wort an den veränderten Text angehängt
   }
 
   public static String jumbleText(Scanner scanner){
