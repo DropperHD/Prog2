ff63d98c22a0b6473c2f62358ba41ef08f4d5394
DropperHD <30983123+DropperHD@users.noreply.github.com>
2020-07-04 16:33:55 +0200

    update

--- Prog2-5/Gamelib/src/gameslib/Platform.java
+++ Prog2-5/Gamelib/src/gameslib/Platform.java
@@ -1,4 +1,40 @@
 package gameslib;
 
-public class Platform {
+import java.util.Objects;
+
+public class Platform implements Comparable<Platform>{
+    private final String name;
+    private final String company;
+
+    public Platform(String name, String company) {
+        this.name = name;
+        this.company = company;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Platform platform = (Platform) o;
+        return Objects.equals(name, platform.name) &&
+                Objects.equals(company, platform.company);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, company);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    @Override
+    public int compareTo(Platform platform) {
+        return 0;
+    }
 }
